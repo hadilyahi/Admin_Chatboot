be25a74c99b2fc7b2a10db3bddddbd830eec153b
gost1930 <hadbimohamed66@gmail.com>
2024-11-25 17:12:00 +0100

    sideBar:add some code

---
+++ Components/SidBar1.jsx
@@ -0,0 +1,8 @@
+
+const SidBar1 = () => {
+  return (
+    <div>SidBar1</div>
+  )
+}
+
+export default SidBar1
--- Components/SideBar.jsx
+++ Components/SideBar.jsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 
 const navItems = [
   { id: 1, src: "/dashbord.svg", alt: "Dashboard", text: "Dashboard", href: "" },
-  { id: 2, src: "/faQ.svg", alt: "FAQs", text: "FAQs Management", href: "" },
+  { id: 2, src: "/faQ.svg", alt: "FAQs", text: "FAQs", href: "" },
   { id: 3, src: "/Reports.svg", alt: "Reports", text: "Reports", href: "" },
-  { id: 4, src: "/settings.svg", alt: "Settings", text: "Bot Settings", href: "" },
+  { id: 4, src: "/settings.svg", alt: "Settings", text: "Settings", href: "" },
 ];
 
 const Sidebar = () => {
@@ -14,27 +14,30 @@ const Sidebar = () => {
 
   return (
     <div
-      className={`h-screen bg-[#22297C] text-white flex flex-col gap-40 transition-all duration-400 ease-in-out ${
+      className={`h-screen bg-[#22297C] text-white flex flex-col  gap-10 transition-all duration-400 ease-in-out relative ${
         isOpen ? "w-64" : "w-20"
       }`} 
       onMouseEnter={() => setIsOpen(true)} 
       onMouseLeave={() => setIsOpen(false)} 
     >
+      {/* toggle button */}
+      <div className="absolute top-4 right-4">close</div>
+
       <div className="h-20 flex items-center justify-center m-4">
         <Image
           src={"/Logo.jpg"}
           alt={"Logo"}  
-          width={isOpen ? 100 : 60}
-          height={isOpen ? 100 : 60}
-          className="rounded-full transition-all duration-300 ease-in-out"
+          width={isOpen ? 80 : 60}
+          height={isOpen ? 80 : 60}
+          className="rounded-full transition-all duration-400 ease-in-out"
         />
       </div>
 
-      <nav className="space-y-4">
+      <nav className="space-y-14">
         {navItems.map((item) => (
           <div
             key={item.id}
-            className="flex items-center gap-2 mx-4 transition-none" 
+            className="flex items-center gap-2 mx-4  cursor-pointer hover:-translate-y-2 duration-200 ease-in-out" 
           >
             <Image
               src={item.src}
@@ -44,7 +47,7 @@ const Sidebar = () => {
               className="flex-shrink-0"
             />
             {isOpen && (
-              <span className="text-[#FECC0B] font-itim text-2xl">
+              <span className="text-[#FECC0B] hover:underline font-itim text-2xl text-center duration-500">
                 {item.text}
               </span>
             )}
