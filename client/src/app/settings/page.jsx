bc88211dfd1edf9191d15544858429a0d7c73ff7
ahmedRAOUANE <ahmedraouane30@gmail.com>
2024-11-26 15:43:39 +0100

    update sidebar and uild the routning system

---
+++ client/src/app/settings/page.jsx
@@ -0,0 +1,9 @@
+const SettingsPage = () => {
+    return (
+        <main className="flex-1">
+            <h1 className='text-3xl'>Settings</h1>
+        </main>
+    )
+}
+
+export default SettingsPage
