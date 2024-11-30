1e89351b756ff8212581b8d091fc56edb7e1c4d2
Your Name <frocodeof@gmail.com>
2024-11-30 23:54:14 +0100

    ++

--- back_end/routes/questionRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { createQuestion } = require("./../controllers/questionController");
-const router = express.Router();
-
-router.post("/create-question/:workflowId", createQuestion);
-
-module.exports = router;
